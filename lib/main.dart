9c6619284a20a463f38a6f271995baf00785611d
Morel-D <morelDen@gmail.com>
2023-01-10 11:39:39 -0800

    Profile head

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello"),
-        ),
 
-        body: Center(
-          child: Text('Hello'),
-        ),
+        body: Column(
+
+          children: [
+            Container(
+              padding: EdgeInsets.fromLTRB(20, 80, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                 Container(
+
+                   child: Column(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                     children: [
+                       Text("Welcome", style: TextStyle(color: Colors.grey),),
+                       Text("Mairane Jane", style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold))
+                     ],
+                   ),
+                 ),
+                  SizedBox(width: 150),
+                  Container(
+                    padding: EdgeInsets.all(25),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/Photo2.png"),
+                      )
+                    ),
+                  ),
+
+                ],
+              ),
+            )
+          ],
+        )
       ),
     );
   }
